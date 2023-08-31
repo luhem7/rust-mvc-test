0e88a4a163c15eaa1396d11c9fd6e6fca361dca3
luhem7 <mehul.gang@gmail.com>
2023-08-31 19:19:32 -0400

    WIP reading input from stdin

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{
     thread,
     sync::{Arc,Mutex},
     time::Duration,
+    io
 };
 
 use clearscreen::clear;
@@ -22,16 +23,21 @@ impl Model {
     }
 }
 
-struct View;
+struct View {
+    last_user_input: String
+}
 
 impl View {
     fn new() -> Self {
-        Self
+        Self {
+            last_user_input: "".to_string()
+        }
     }
 
     fn render_view(&self, model: &mut Model) {
         clear().expect("failed to clear screen");
         println!("Rendering View {}", model.counter);
+        println!("Last user input: {}", self.last_user_input);
     }
 }
 
@@ -44,17 +50,27 @@ fn main() {
     let model_arc = Arc::new(Mutex::new(Model::new()));
     let model_arc_clone = Arc::clone(&model_arc);
 
-    loop {
+    // Rendering the view
+    thread::spawn(move || loop {
         let view = view_arc_clone.lock().unwrap();
         let mut model = model_arc_clone.lock().unwrap();
         model.increment_counter();
         view.render_view(&mut model);
         thread::sleep(Duration::from_millis(1000));
-    }
+    });
 
-    // thread::spawn(move || loop {
-    //     let mut view = view_arc_clone.lock().unwrap();
-    //     view.render_view();
-    //     thread::sleep(Duration::from_millis(1000));
-    // });
+    // Handling user input
+    let view_arc_clone = Arc::clone(&view_arc);
+    loop {
+        thread::sleep(Duration::from_millis(500));
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        if input.is_empty() { continue; }
+        else if input == "q" { break; }
+        else {
+            let mut view = view_arc_clone.lock().unwrap();
+            view.last_user_input = input; 
+        }
+    }
 }
